1bca3f782c979fdd25793f8e6600a16ecc7d2edc
Corey Merritt <coreyleemerritt@gmail.com>
2024-03-27 16:03:53 -0400

    Added a new sed algorithm to allow for more robust formatting.

--- readall.sh
+++ readall.sh
@@ -9,8 +9,14 @@ SELECT entry_number, date_time, entry
 FROM entries;
 "
 
-mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p -D "$MYSQL_DATABASE" -e "$SQL" | grep -v '^[-]*$' > output
+mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p -D "$MYSQL_DATABASE" -e "$SQL" | grep -v '^[-]*$' > output_dirty
 
-sed -i '/^[0-9]+\s[0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}/ s/\([0-9]\)\t\([0-9]\)/\n\1\t\2\n/g; s/$/\n/' output
+sed -i 's/^\([0-9]\+\s[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\s[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\)/\n\1\n/g' output_dirty
+sed -i 's/\\t/\t/g' output_dirty
+sed -i 's/\\n/\n/g' output_dirty
+
+cp output_dirty output
+rm output_dirty
 
 vim -c ":set wrap linebreak" +$ output
+
--- readlast.sh
+++ readlast.sh
@@ -12,6 +12,13 @@ WHERE entry_number = (
 	FROM entries
 	);"
 
-mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p -D "$MYSQL_DATABASE" -e "$SQL" | grep -v '^[-]*$' > output
+mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p -D "$MYSQL_DATABASE" -e "$SQL" | grep -v '^[-]*$' > output_dirty
+
+sed -i 's/^\([0-9]\+\s[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\s[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\)/\n\1\n/g' output_dirty
+sed -i 's/\\t/\t/g' output_dirty
+sed -i 's/\\n/\n/g' output_dirty
+
+cp output_dirty output
+rm output_dirty
 
 vim -c ":set wrap linebreak" output
