a36a46839d1270bed526ee00537027fa504df8e6
Corey Merritt <coreyleemerritt@gmail.com>
2024-03-27 17:10:26 -0400

    Added a couple newlines to clearly seperate entries.

--- readspecifc.sh
+++ readspecifc.sh
@@ -28,7 +28,7 @@ fi
 
 mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p -D "$MYSQL_DATABASE" -e "$SQL" | grep -v '^[-]*$' > output_dirty
 
-sed -i 's/^\([0-9]\+\s[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\s[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\)/\n\1\n/g' output_dirty
+sed -i 's/^\([0-9]\+\s[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\s[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\)/\n\n\n\1\n/g' output_dirty
 sed -i 's/\\t/\t/g' output_dirty
 sed -i 's/\\n/\n/g' output_dirty
 
