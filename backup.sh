28eb2c4023546a81297f51807229553159b0ade9
Corey Merritt <coreyleemerritt@gmail.com>
2024-03-28 08:27:55 -0400

    Renamed this tool and added some user feedback.

---
+++ backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mysqldump -u root -p blog_entries > blog_entries.sql
+
+cur_time=$(date +%S)
+mod_time=$(stat -c %Y "blog_entries.sql")
+time_dif=$((cur_time - mod_time))
+
+if [ "$time_dif" -le 2 ]; then
+	echo "Backup Successful."
+else
+	echo "blog_entries.sql was not updated, something went wrong."
+fi
