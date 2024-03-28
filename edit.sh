9a9d86c3e69f8308f836b346e94136643b8161b1
Corey Merritt <coreyleemerritt@gmail.com>
2024-03-28 10:52:56 -0400

    Edit is now functional. Fixed a bug where 2nd arg wasn't being passed to blog_edit().

--- edit.sh
+++ edit.sh
@@ -2,28 +2,36 @@
 
 function blog_edit() {
 
-	echo "Which entry would you like to overwrite?:"
-	read OVERWRITE_NUM
+	if [ "$1" == "" ]; then
+		echo "Which entry would you like to overwrite?:"
+		read OVERWRITE_NUM
+	else
+		OVERWRITE_NUM="$1"
+	fi
 
-	cp entry entry_mysql
+	if [ "$2" == "" ]; then
+                file_name="./entry"
+        else
+                file_name="$2"
+        fi
 
-	sed -i "s/'/', CHAR(39), '/g" entry_mysql
-	sed -i "s/\"/', CHAR(34), '/g" entry_mysql
-	sed -i "s/;/', CHAR(59), '/g" entry_mysql
+        cp "$file_name" entry_dirty
 
-	ENTRY=$(<entry_mysql)
-	rm entry_mysql
+	sed -i "s/'/', CHAR(39), '/g" entry_dirty
+	sed -i "s/\"/', CHAR(34), '/g" entry_dirty
+	sed -i "s/;/', CHAR(59), '/g" entry_dirty
 
-<<COMMENT
-MYSQL_HOST="localhost"
-MYSQL_USER="root"
-MYSQL_DATABASE="blog_entries"
-COMMENT
+	ENTRY=$(<entry_dirty)
+	rm entry_dirty
 
 	SQL="
 		UPDATE entries
 		SET entry = CONCAT('$ENTRY')
 		WHERE (entry_number = $OVERWRITE_NUM);
+	
+		SELECT *
+                FROM entries
+                WHERE entry_number = $OVERWRITE_NUM;
 	"
 
 	echo "Are you sure you want to permanently overwrite this entry?: (Y/N)"
@@ -31,21 +39,34 @@ COMMENT
 
 
 	if [ $EDIT_ANS == "Y" ] || [ $EDIT_ANS == "y" ]; then
-		mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p -D "$MYSQL_DATABASE" -e "$SQL"
+		mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p -D "$MYSQL_DATABASE" -e "$SQL" | grep -v '^[-]*$' > output_dirty
+		
+		if [ $? -eq 0 ]; then
+
+        		sed -i 's/^\([0-9]\+\s[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\s[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\)/\n\n\n\1\n/g' output_dirty
+        		sed -i 's/\\t/\t/g' output_dirty
+        		sed -i 's/\\n/\n/g' output_dirty
+
+        		cp output_dirty output
+        		rm output_dirty
+
+        		echo "Read last entry?: (Y/N)"
+        		read READ_ANS
+
+        		if [ "$READ_ANS" == "Y" ] || [ "$READ_ANS" == "y" ]; then
+                		vim output
+				echo "Overwrite completed."
+        		else
+				echo "Overwrite completed."
+            	   		exit 0
+			fi
+		else
+                	echo "Nothing has been overwritten."
+			exit 1
+		fi
 	else
-		exit 1
+		echo "Nothing has been overwritten."
+                        exit 0
 	fi
-
-<<COMMENT
-echo "Read all entries?: (Y/N)"
-read READ_ANS
-
-
-if [ "$READ_ANS" == "Y" ] || [ "$READ_ANS" == "y" ]; then
-	./read.sh all
-elif [ "$READ_ANS" == "N" ] || [ "$READ_ANS" == "n" ]; then
-	exit 1
-fi
-COMMENT
 
 }
