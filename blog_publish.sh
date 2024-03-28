78702c9e6ad352ede1672943a9fc42e680e76fdf
Corey Merritt <coreyleemerritt@gmail.com>
2024-03-28 12:22:19 -0400

    Publish tool is functional.

---
+++ blog_publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function publish_tools() {
+	
+		cp blog_read.sh /usr/local/bin/blog_read.sh
+		cp blog_write.sh /usr/local/bin/blog_write.sh
+		cp blog_edit.sh /usr/local/bin/blog_edit.sh
+		cp blog_delete.sh /usr/local/bin/blog_delete.sh
+		cp blog_backup.sh /usr/local/bin/blog_backup.sh
+		cp blog_publish.sh /usr/local/bin/blog_publish.sh
+		cp blog /usr/local/bin/blog
+
+}
