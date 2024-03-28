8b5b7b7ccc5793b086a87a20639263f2359beae1
Corey Merritt <coreyleemerritt@gmail.com>
2024-03-28 11:11:12 -0400

    Delete is in working condition and now has some additional functionality.

--- delete.sh
+++ delete.sh
@@ -2,18 +2,19 @@
 
 function blog_delete() {
 
-	echo "Which entries would you like to delete? ex)1, 2, 5"
-	read DEL_NUMS
-
-<<COMMENT
-	MYSQL_HOST="localhost"
-	MYSQL_USER="root"
-	MYSQL_DATABASE="blog_entries"
-COMMENT
+	if [ "$1" == "" ]; then
+                echo "Which entry would you like to delete?:   ex)1, 2, 5"
+                read DEL_NUMS
+        else
+                DEL_NUMS="$1"
+        fi
 
 	SQL="
 		DELETE FROM entries
-		WHERE entry_number IN ($DEL_NUMS); 
+		WHERE entry_number IN ($DEL_NUMS);
+
+                SELECT *
+                FROM entries; 
 	"
 
 	echo "Are you sure you want to permanently delete these entries?: (Y/N)"
@@ -21,21 +22,35 @@ COMMENT
 
 
 	if [ $DEL_ANS == "Y" ] || [ $DEL_ANS == "y" ]; then
-		mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p -D "$MYSQL_DATABASE" -e "$SQL"
+		
+		mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p -D "$MYSQL_DATABASE" -e "$SQL" | grep -v '^[-]*$' > output_dirty
+
+                if [ $? -eq 0 ]; then
+
+                        sed -i 's/^\([0-9]\+\s[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\s[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\)/\n\n\n\1\n/g' output_dirty
+                        sed -i 's/\\t/\t/g' output_dirty
+                        sed -i 's/\\n/\n/g' output_dirty
+
+                        cp output_dirty output
+                        rm output_dirty
+
+                        echo "Read all entries?: (Y/N)"
+                        read READ_ANS
+
+                        if [ "$READ_ANS" == "Y" ] || [ "$READ_ANS" == "y" ]; then
+                                vim output
+                                echo "Deletion completed."
+				exit 0 
+                        else
+                                echo "Deletion completed."
+                                exit 0
+                        fi
+                else
+                        echo "Nothing has been deleted."
+                        exit 1
+                fi
 	else	
 		exit 1
 	fi
 
 }
-
-<<COMMENT
-echo "Read all entries?: (Y/N)"
-read READ_ANS
-
-
-if [ "$READ_ANS" == "Y" ] || [ "$READ_ANS" == "y" ]; then
-	./readall.sh
-elif [ "$READ_ANS" == "N" ] || [ "$READ_ANS" == "n" ]; then
-	exit 1
-fi
-COMMENT
